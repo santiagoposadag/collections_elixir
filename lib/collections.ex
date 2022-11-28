1e52c91bd2113a22af32272680919c15fb50ebc7
Santiago Posada <santiago90dell@gmail.com>
2022-11-28 11:39:57 -0500

    exercises ready

--- lib/collections.ex
+++ lib/collections.ex
@@ -1,4 +1,5 @@
 defmodule Collections do
+alias Inspect.MapSet
 
   """
   Recibir un input del usuario en fomrato string,
@@ -53,16 +54,28 @@ defmodule Collections do
   """
 
   def working_with_list1 do
-    :world
+    cicle = [0, 1, 2, 3 ,4]
+    list = Enum.map(cicle, fn _x -> IO.gets("ingrese una palabra ") end)
+    IO.puts("Esta es la lista que has ingresado")
+    IO.write(list)
+    listInUppercase = Enum.map(list, fn x -> String.upcase(x) end)
+    IO.puts("Este es el resultado")
+    listInUppercase
   end
 
   """
   Construir a mano dos listas que contengan elementos iguales,
   restarle a la primera lista los valores en comun de la segunda lista
-  y por ultimo imprimir la cabeza y la cola de la lista
+  y por ultimo imprimir la cabeza y la cola de la primera lista
   """
   def working_with_list2 do
-    :world
+    lista1 = ["foo-", "barr-", "groo-", "rizz-", "malone-"]
+    lista2 = ["ecto-", "foo-", "groo-"]
+    result = lista1 -- lista2
+    IO.puts("este es el resultado = #{result}")
+    [head | tail] = result
+    IO.puts("Esta es la cabeza = #{head}")
+    IO.puts("Esta es la cola = #{tail}")
   end
 
   """
@@ -71,24 +84,34 @@ defmodule Collections do
   recorre el mapa y sumale 5 a cada uno de los valores, al final imprime el reultado
   """
   def working_with_map1 do
-    :world
+    map = %{one: 1, two: 3, three: 2, four: 5}
+    mapResult = Enum.map(map, fn ({x, y}) -> {x,  y + 5}  end)
+    mapResult
   end
 
   """
-  Constrir un mapa, a partir de el contruir una lista e imprimir el resultado
+  Constrir un mapa, a partir de el contruir una lista solo con los valores
+  e imprimir el resultado
   """
   def working_with_map2 do
-    :world
+    map = %{one: "santiago", two: "camilo", three: "juan", four: "pedro"}
+    listResult = Enum.map(map, fn ({_x, y}) -> y  end)
+    listResult
   end
 
   """
-  Utilizar la estructura de datos, construir dos mapsets
-  manualmente, y quitarle los elementos del segundo mas set al primero que tengan en comun
-  por medio de las funciones del map set
-  (para que esto sea posible el map set uno debe tener elementos en comun con el map set dos)
+  Recibe tres palabras por consola, esto ubicalo en una lista,
+  y por medio de el modulo Enum, transforma la lista para que cada palabra se repita 3 veces
+  y que aparezca en un solo nivelm es decir, que no hayan listas anidadas
   """
-  def working_with_mapset1 do
-    :world
+  def working_with_list3 do
+    cicle = [0, 1, 2]
+    list = Enum.map(cicle, fn _x -> IO.gets("ingrese una palabra ") end)
+    IO.puts("Esta es la lista que has ingresado")
+    IO.write(list)
+    result = Enum.map(list, fn x -> Enum.map(cicle, fn _y -> x end) end)
+    result = List.flatten(result)
+    result
   end
 
   """
@@ -96,7 +119,8 @@ defmodule Collections do
   saltando de 3 en 3 e imprimir el resultado.
   """
   def working_with_range1 do
-    :world
+    result = Enum.to_list(0..20//3)
+    result
   end
 
   """
@@ -105,7 +129,9 @@ defmodule Collections do
   imprimir el resultado
   """
   def working_with_stream1 do
-    :world
+    range = 0..10//2
+    result = Enum.map(range, fn x -> x + 3 end)
+    result
   end
 
 

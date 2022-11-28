c323ea067de604ff5f1682431edd0428cb53c35c
Santiago Posada <santiago90dell@gmail.com>
2022-11-28 11:42:22 -0500

    statements ready and fixed

--- lib/collections.ex
+++ lib/collections.ex
@@ -7,18 +7,16 @@ defmodule Collections do
   """
 
   def working_with_tuples1 do
-    :world
   end
 
   """
   Tomar una tupla que contenga un string en su primera posicion,
-  agregarle mas valores, convertirla en un List y manipularla para que
+  agregarle mas valores, convertirla en un list y manipularla para que
   queden todas las posiciones en mayuscula,
   al final imprimir el resultado
   """
 
   def working_with_tuples2 do
-    :world
   end
 
   """
@@ -27,7 +25,6 @@ defmodule Collections do
   """
 
   def working_with_keywordlist1 do
-    :world
   end
 
   """
@@ -37,16 +34,14 @@ defmodule Collections do
   """
 
   def working_with_list1 do
-    :world
   end
 
   """
-  Construir a mano dos listas que contengan elementos en comun,
+  Construir a mano dos listas que contengan elementos iguales,
   restarle a la primera lista los valores en comun de la segunda lista
-  y por ultimo imprimir la cabeza y la cola de la lista
+  y por ultimo imprimir la cabeza y la cola de la primera lista
   """
   def working_with_list2 do
-    :world
   end
 
   """
@@ -55,26 +50,21 @@ defmodule Collections do
   recorre el mapa y sumale 5 a cada uno de los valores, al final imprime el reultado
   """
   def working_with_map1 do
-    :world
   end
 
   """
-  Constrir un mapa, a partir de el contruir una lista,
-  manipular los valores para que esten en mayuscula
+  Constrir un mapa, a partir de el contruir una lista solo con los valores
   e imprimir el resultado
   """
   def working_with_map2 do
-    :world
   end
 
   """
-  Construir dos mapsets
-  manualmente, y quitarle los elementos del segundo map set al primero que tengan en comun
-  por medio de las funciones del map set
-  (para que esto sea posible el map set uno debe tener elementos en comun con el map set dos)
+  Recibe tres palabras por consola, esto ubicalo en una lista,
+  y por medio de el modulo Enum, transforma la lista para que cada palabra se repita 3 veces
+  y que aparezca en un solo nivelm es decir, que no hayan listas anidadas
   """
-  def working_with_mapset1 do
-    :world
+  def working_with_list3 do
   end
 
   """
@@ -82,17 +72,16 @@ defmodule Collections do
   saltando de 3 en 3 e imprimir el resultado.
   """
   def working_with_range1 do
-    :world
   end
 
   """
-  Construir un rango de 0 a 10 saltando de 2 en 2,
+  Cosntruir un rango de 0 a 10 saltando de 2 en 2,
   convertir a lista, sumarle 3 a cada vlor e
   imprimir el resultado
   """
   def working_with_stream1 do
-    :world
   end
+
 
 
 end
